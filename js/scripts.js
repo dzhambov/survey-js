23898dc7164cbc9cfbdee78415e88e477d0a3a33
Student <example@example.com>
2019-10-25 13:29:06 -0700

    Add troubleshooting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-  $("").submit(function(event) {
+  $("form.language").submit(function(event) {
+    var value = $("#language option:selected")
     event.preventDefault();
 
+    if (value === "ruby") {
+      $("python").hide();
+      $("c#").hide();
+      $("ruby").show();
+    } else (value === "python") {
+      $("ruby").hide();
+      $("c#").hide();
+      $("python").show();
+    } if ( value === "c#"") {
+      $("ruby").hide();
+      $("python").hide();
+      $("c#").show();
 
+    }
 
   });
 
