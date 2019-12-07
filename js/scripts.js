6bfd7228575c5289711a7841676829d7f97f3015
Hristo Dzhambov <dzhambov@hotmail.com>
2019-12-07 14:13:15 -0800

    Cleaner code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    var color = parseInt($("#color option:selected").val());
-    var browser = parseInt($("#browser option:selected").val());
-    var pc = parseInt($("#pc option:selected").val());
-    var city = parseInt($("#city option:selected").val());
-    var lego = parseInt($("#lego option:selected").val());
-    var result = (color + browser + pc + city + lego);
     event.preventDefault();
-    // var result = calculate ();
-    // console.log("result is:" + result)
-
+    var color = parseInt($("#color").val());
+    var browser = parseInt($("#browser").val());
+    var pc = parseInt($("#pc").val());
+    var city = parseInt($("#city").val());
+    var lego = parseInt($("#lego").val());
+    var result = (color + browser + pc + city + lego);
+    
   if (result <= 7) {
     $("#csharp").show();
     $("#python, #ruby").hide();
@@ -18,16 +16,13 @@ $(document).ready(function() {
       $("#python").show();
       $("#csharp, #ruby").hide();
 
-  } else if (result < 22) {
+  } else if (result <= 22) {
       $("#ruby").show();
       $("#csharp, #python").hide();
 
       return (result);
+
   };
 });
-
-
-
-
 
 });
